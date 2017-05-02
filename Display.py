7226da54121beaca38e3784fc59566e643613881
Ronen Gilead-Raz <ronengi@gmail.com>
2017-05-02 21:48:32 +0300

    Add initial python classes

---
+++ Display.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+
+class Display:
+    """Represents the way something interacts with the outside world.
+    
+    """
+
+    def __init__(self, text_direction="ltr", language="en"):
+
---
+++ InfoTom.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+class InfoTom:
+    """Short for Information Atom. Represents a basic unit of information.
+
+    
+    def __init__(self, parent=None, title=None, contents=None, examples=None, level=0, key_words=None, display=None):
+        self.parent = parent
+
+
+
+
+
+
+    def __str__(self):
+
+
+
+
+
+
+
+
+
+
+
+
+    def valid(self):
+    
