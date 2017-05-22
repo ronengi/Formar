13e28668d93f2ee7e4ebafc3d10cac4ba8f4e4a6
Ronen Gilead-Raz <ronengi@gmail.com>
2017-05-22 18:42:55 +0300

    Change structure of Bond
      Add b_id
      Add level
      Add test cases to Bond

--- formar/fdata/Bond.py
+++ formar/fdata/Bond.py
@@ -23,17 +23,36 @@ from formar.fdata.InfoCompound import InfoCompound
 
 class Bond:
 
-    def __init__(self, infotom1=None, infotom2=None, description=''):
+    def __init__(self, b_id=0, infotom1=None, infotom2=None, level=0, description=''):
+        self.__b_id = b_id
         self.__infotom1 = infotom1
         self.__infotom2 = infotom2
+        self.__level = level
         self.__description = description
 
-    def get_description(self):
+    def __str__(self):
         try:
             return str(self.__description)
         except (AttributeError, TypeError):
             # no such attribute or None
             return ''
+
+    def __eq__(self, other):
+        if not isinstance(other, Bond):
+            return False
+        if self.get_infotom1() == other.get_infotom1() \
+                and self.get_infotom2() == other.get_infotom2() \
+                and self.get_level() == other.get_level() \
+                and self.get_description() == other.get_description():
+            return True
+        return False
+
+    def get_b_id(self):
+        try:
+            return self.__b_id
+        except (AttributeError, TypeError):
+            # no such attribute or None
+            return None
 
     def get_infotom1(self):
         try:
@@ -49,8 +68,23 @@ class Bond:
             # no such attribute or None
             return None
 
+    def get_level(self):
+        try:
+            return int(self.__level)
+        except (AttributeError, TypeError):
+            # no such attribute or None
+            return 0
+
+    def get_description(self):
+        try:
+            return str(self.__description)
+        except (AttributeError, TypeError):
+            # no such attribute or None
+            return ''
+
     def encode(self):
         return dict(__Bond__=dict(
+            b_id=self.get_b_id(),
             infotom1=self.get_infotom1(),
             infotom2=self.get_infotom2(),
             description=self.get_description()))
@@ -61,6 +95,11 @@ class Bond:
             j_dict_bond = j_dict['__Bond__']
         except KeyError:
             return None
+
+        try:
+            b_b_id = j_dict_bond['b_id']
+        except KeyError:
+            b_b_id = None
 
         try:
             b_infotom1 = j_dict_bond['infotom1']
@@ -77,7 +116,7 @@ class Bond:
         except KeyError:
             b_description = ''
 
-        b = Bond(infotom1=b_infotom1, infotom2=b_infotom2
+        b = Bond(b_id=b_b_id, infotom1=b_infotom1, infotom2=b_infotom2
                  , description=b_description)
         return b
 
---
+++ formar/tests/TestBond.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import unittest
+from formar.fdata.Bond import Bond
+from formar.fdata.InfoTom import InfoTom
+
+
+class TestBond(unittest.TestCase):
+
+    def test_str(self):
+        b = Bond(b_id=123, infotom1=456, infotom2=789, description='vim:Configuration')
+        self.assertEqual(str(b), 'vim:Configuration')
+
+    def test_encode(self):
+        b = Bond(b_id=123, infotom1=456, infotom2=789, description='vim:Configuration')
+        j_dict = {'__Bond__': {
+            'b_id': 123
+            , 'infotom1': 456, 'infotom2': 789
+            , 'description': 'vim:Configuration'}}
+        self.assertEqual(b.encode(), j_dict)
+
+    def test_decode(self):
+        b = Bond(b_id=123, infotom1=456, infotom2=789, description='vim:Configuration')
+        j_dict = {'__Bond__': {
+            'b_id': 123
+            , 'infotom1': 456, 'infotom2': 789
+            , 'description': 'vim:Configuration'}}
+        self.assertEqual(Bond.decode(j_dict), b)
+
+if __name__ == '__main__':
+    unittest.main()
+
